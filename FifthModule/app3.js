a669fa0f85ba03d1ae04cf7d0eb2a8d8e939ab78
raghavddps2 <raghav.ddps2@gmail.com>
2019-03-31 04:00:16 +0530

    Adding changes

--- FifthModule/app3.js
+++ FifthModule/app3.js
@@ -3,10 +3,12 @@ const express = require('express');
 const bodyParser =require('body-parser');
 const app = express();
 
+const rootDir = require('./util/path');
+
 const adminRoutes = require('./routes/admin'); //Just importing the rotes we created.
 const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({extended: false}));
-
+app.use(express.static(path.join(__dirname,'public')));
 
 //This will parse the url sent.
 app.use('/admin',adminRoutes); //Just directly using like a function.
@@ -15,7 +17,7 @@ app.use(shopRoutes); //Just directly use like a function
 //admin/add-product ==> GET
 //admin/product ==>POST
 app.use((req,res,next)=>{
-    res.status(404).sendFile(path.join(__dirname,'views','404.html')); //We  can even et the status code.
+    res.status(404).sendFile(path.join(rootDir,'views','404.html')); //We  can even et the status code.
 })
 
 app.listen(3000);
